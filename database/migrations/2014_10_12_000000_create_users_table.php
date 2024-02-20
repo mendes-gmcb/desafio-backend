7a072f5c7de06327f9419d31b55e1928104ab8bb
gmcb mendes <gmendescamargodebarros@gmail.com>
2024-02-19 22:16:29 -0300

    first commit - base laravel

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(uuid_create(4));
+            $table->enum('type', ['store', 'user'])->default('user');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('cpf_cnpj', 18)->unique();
+            $table->string('password');
+            $table->bigInteger('balance')->default(0);
+            $table->bigInteger('limit')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
