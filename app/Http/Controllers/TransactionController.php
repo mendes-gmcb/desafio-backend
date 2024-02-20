b492f593a51c63a8dde1c70408f4a2c40c718e46
gmcb mendes <gmendescamargodebarros@gmail.com>
2024-02-19 23:38:48 -0300

    create a valid transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -18,5 +18,24 @@ class TransactionController extends Controller
             'description' => ['required', 'string', 'max:255'],
         ]);
         // return response(200);
+
+        $payer = User::where('id', $request->payer)->firstOrFail();
+        $payee = User::where('id', $request->payee)->firstOrFail();
+        // return response()->json([$payer, $payee]);
+
+        // Verifica se o usuário que está enviando a transferência é um usuário comum
+        // Verifica se o usuário que está enviando a transferência tem saldo suficiente
+        // Consulta o serviço autorizador externo
+        // Verifica se a consulta foi bem-sucedida
+        // Verifica se a transferência foi autorizada
+            // Realiza a transferência de dinheiro
+            $transaction = Transaction::create([
+                'payer' => $payer->id,
+                'payee' => $payee->id,
+                'value' => $request->value,
+                'type' => $request->type,
+                'description' => $request->description,
+            ]);
+            return response()->json($transaction, 201);
     }
 }
