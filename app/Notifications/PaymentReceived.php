0c4aaca4d90a10506265a6651bfc83f22b9ab284
gmcb mendes <gmendescamargodebarros@gmail.com>
2024-02-19 23:55:28 -0300

    create payment notification

---
+++ app/Notifications/PaymentReceived.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PaymentReceived extends Notification
+{
+    use Queueable;
+
+    public $transaction;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($transaction)
+    {
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Pagamento Recebido')
+            ->line('Você recebeu um pagamento de ' . $this->transaction->value . ' reais.')
+            ->line('Descrição: ' . $this->transaction->description);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
