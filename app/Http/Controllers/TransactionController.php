ba8916de6d397173ec3727fbc769ce813ab5f308
gmcb mendes <gmendescamargodebarros@gmail.com>
2024-02-19 23:34:04 -0300

    validate request

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,5 +9,14 @@ class TransactionController extends Controller
 {
     public function transactMoney(Request $request)
     {
+        // Valida os dados da requisição
+        $request->validate([
+            'payer' => ['required', 'uuid', 'exists:users,id'],
+            'payee' => ['required', 'uuid', 'exists:users,id'],
+            'value' => ['required', 'int', 'min:1'],
+            'type' => ['required', 'in:c,d,p'],
+            'description' => ['required', 'string', 'max:255'],
+        ]);
+        // return response(200);
     }
 }
